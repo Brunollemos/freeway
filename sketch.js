9eedd6882abd85a3e09d2fe49b7b579f3b8fe472
Bruno Lemos <brunollemoscontato@gmail.com>
2022-11-09 17:32:59 -0300

    Exibindo ator-1 e carro-1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
 
 function preload() {
   imagemDaEstrada = loadImage("img/estrada.png");
+  imagemDoAtor = loadImage("img/ator-1.png");
+  imagemCarro = loadImage("img/carro-1.png");
 }
 
 function setup() {
@@ -10,4 +14,6 @@ function setup() {
 
 function draw() {
   background(imagemDaEstrada);
+  image(imagemDoAtor, 100, 370, 30, 25);
+  image(imagemCarro, 420, 40, 50, 40);
 }
