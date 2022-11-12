7c8ad11285fb42e6a801fb2e05c99c6088840f68
Bruno Lemos <brunollemoscontato@gmail.com>
2022-11-12 08:41:07 -0300

    Adicionando mais carros

--- js/carro.js
+++ js/carro.js
@@ -1,7 +1,7 @@
 //variáveis do carro
-let xCarros = [500, 500, 500];
-let yCarros = [40, 95, 150];
-let velocidadeCarros = [2, 2.5, 3.2];
+let xCarros = [500, 500, 500, 500, 500, 500];
+let yCarros = [40, 95, 150, 210, 270, 318];
+let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 2.3];
 let comprimentoCarro = 50;
 let alturaCarro = 40;
 
